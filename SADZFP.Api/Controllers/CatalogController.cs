0653d5e89ff0b017916deb8a1cfcf1a4feaf801b
Dominic Faiella <faiella.26@osu.edu>
2023-03-29 21:05:25 -0400

    Stopped on page 9. Do not get two URLs back

---
+++ SADZFP.Api/Controllers/CatalogController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using SADZFP.domain.Catalog;
+
+namespace SADZFP.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+
+    }
+}
+    [HttpGet]
+    public IActionResult GetItems()
+    {
+        return Ok("hello world.");
+    }
