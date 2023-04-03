ca7777b7a6017aa63fc3e7dc0653cbb48bba74e4
Dominic Faiella <faiella.26@osu.edu>
2023-04-03 10:10:46 -0400

    API set and works, stopped on Data Access

--- SADZFP.Api/Controllers/CatalogController.cs
+++ SADZFP.Api/Controllers/CatalogController.cs
@@ -7,26 +7,52 @@ namespace SADZFP.Api.Controller
     [Route("[controller]")]
     public class CatalogController : ControllerBase
     {
+        [HttpGet("/catalog")]
+        public IActionResult GetItems()
+        {
+            var items = new List<Item>()
+            {
+                new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+                new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+            };
+            return Ok(items);
+        }
 
         [HttpGet("{id:int}")]
         public IActionResult GetItem(int id)
         {
-            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
-            item.Id = id;
+            var Item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            Item.Id = id;
 
-            return Ok(item);
-    }
-        
+            return Ok(Item);
+        }
+
         [HttpPost("/catalog")]
         public IActionResult Post(Item item)
         {
             return Created("/catalog/42", item);
-}  
- public class HTTPGetController : Controller
-{  
-    [HttpGet("/catalog")]
-    public IActionResult GetItems()
-    {
-        return Ok("hello world.");
+        }
+
+        [HttpPost("{id:int}/ratings")]
+        public IActionResult PostRating(int id, [FromBody] Rating rating)
+        {
+            var Item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            Item.Id = id;
+            Item.AddRating(rating);
+
+            return Ok(Item);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Item item)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
     }
-}
+}  
