f6910a114ecbe5ccbf00b2de36fdc5b59889ad33
Dominic Faiella <faiella.26@osu.edu>
2023-04-03 10:09:54 -0400

    API set and works, stopped on Data Access

--- SADZFP.Api/Controllers/CatalogController.cs
+++ SADZFP.Api/Controllers/CatalogController.cs
@@ -8,7 +8,19 @@ namespace SADZFP.Api.Controller
     public class CatalogController : ControllerBase
     {
 
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id)
+        {
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.Id = id;
+
+            return Ok(item);
     }
+        
+        [HttpPost("/catalog")]
+        public IActionResult Post(Item item)
+        {
+            return Created("/catalog/42", item);
 }  
  public class HTTPGetController : Controller
 {  
