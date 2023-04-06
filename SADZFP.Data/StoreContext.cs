7d657e07e3fef7f32cd03ed40ec52cf5f8028c11
Alex-Peffley <apeff8@gmail.com>
2023-04-05 22:53:13 -0400

    Finished up the last few pages of lab 4

--- SADZFP.Data/StoreContext.cs
+++ SADZFP.Data/StoreContext.cs
@@ -11,6 +11,8 @@ namespace SADZFP.Data
 
         public DbSet<Item> Items { get; set; }
 
+        public DbSet<Order> Orders { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
