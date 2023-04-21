3eddc39be066106082ac28435e7df50820408769
Dominic Faiella <faiella.26@osu.edu>
2023-04-21 19:16:31 -0400

    Finished pg 2

--- Testing/SADZFP.Domain.Testing/RatingTests.cs
+++ Testing/SADZFP.Domain.Testing/RatingTests.cs
@@ -1,5 +1,5 @@
 namespace SADZFP.Domain.Testing;
-using Rating;
+using SADZFP.domain.Catalog;
 
 [TestClass]
 public class UnitTest1
@@ -7,7 +7,7 @@ public class UnitTest1
     [TestMethod]
     public void Can_Create_New_Rating()
     {
-        var rating = new Rating(1, "Mike". "Great fit!");
+        var rating = new Rating(1, "Mike", "Great fit!");
 
         Assert.AreEqual(1, rating.Stars);
         Assert.AreEqual("Mike", rating.UserName);
